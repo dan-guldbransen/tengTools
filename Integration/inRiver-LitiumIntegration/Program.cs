4f03ecebd24fb4ba46324578c1ff3131f81d200f
LAPTOP-KVUO8IB5\Andreas <andreas.eklund@consid.se>
2021-03-30 16:37:00 +0200

    changed solution to framework and added basic connection

--- Integration/inRiver-LitiumIntegration/Program.cs
+++ Integration/inRiver-LitiumIntegration/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using inRiver.Remoting;
+using inRiver.Remoting.Extension;
+using inRiver.Remoting.Objects;
+using inRiver_LitiumIntegration.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace inRiver_LitiumIntegration
 {
@@ -6,7 +14,24 @@ namespace inRiver_LitiumIntegration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string InRiverRemotingUrl = "https://remoting.productmarketingcloud.com";
+            const string InRiverUsername = "inriver-tengtools@consid.se";
+            const string InRiverPassword = "544%%IdkwDWHk\"XgbeU3pdD"; //notera escapat " i lösen... Autogenerat :)
+            const string InRiverEnvironmentProd = "prod";
+            const string InRiverEnvironmentTest = "test";
+
+            const string ProductTypeId = "Product";
+            const string ItemTypeId = "Item";
+            const string ResourceTypeId = "Resource";
+
+
+            Console.WriteLine("Connecting...");
+            var context = new inRiverContext(RemoteManager.CreateInstance(InRiverRemotingUrl, InRiverUsername, InRiverPassword, InRiverEnvironmentTest), new ConsoleLogger());
+            Console.WriteLine("Connected!");
+
+            //Välj det sättet  som passar er bäst, med optimering osv
+            var productId = context.ExtensionManager.DataService.GetAllEntityIdsForEntityType(ProductTypeId);
+            var products = context.ExtensionManager.DataService.GetEntitiesForEntityType(0, ProductTypeId, LoadLevel.DataAndLinks);
         }
     }
 }
