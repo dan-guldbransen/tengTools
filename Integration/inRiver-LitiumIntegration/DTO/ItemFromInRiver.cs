643c1eae2a5161634925e0fc1a2de29b7cc3fe5b
LAPTOP-KVUO8IB5\Andreas <andreas.eklund@consid.se>
2021-04-09 16:27:37 +0200

    Added api calls for imports to Litium Connect

---
+++ Integration/inRiver-LitiumIntegration/DTO/ItemFromInRiver.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inRiver_LitiumIntegration.DTO
+{
+    public class ItemFromInRiver
+    {
+        public string ItemWorkingDescription { get; set; }
+        public string ItemId { get; set; }
+        public string ItemShortDescription { get; set; }
+        public string ItemLongDescription { get; set; }
+        public string ItemStatus { get; set; }
+        public string ItemReplacementsItems { get; set; }
+        public DateTime ItemRegistrationDate { get; set; }
+        public DateTime ItemToBeDiscontinuedDate { get; set; }
+        public DateTime ItemDiscontinuedDate { get; set; }
+        public string ItemApprovedForMarket { get; set; }
+        public string ItemOrderType { get; set; }
+        public string ItemOperation { get; set; }
+        public string ItemWarehouse { get; set; }
+        public bool ItemOnlyFromUKforEU { get; set; }
+        public string ItemCountryOfOrigin { get; set; }
+        public double ItemTarriffCodeEurope { get; set; }
+        public double ItemUNSPSC { get; set; }
+        public double ItemNSN { get; set; }
+        public string ItemStandardType { get; set; }
+        public string ItemCertificates { get; set; }
+        public bool ItemElectricRecycling { get; set; }
+        //public bool Environmental_fee_Y/N { get; set; }
+        public double ItemSellingMinQuantityTW { get; set; }
+        public double ItemOrderMultipleTW { get; set; }
+        public double ItemLunaArticleNumber { get; set; }
+        public string ItemProductOwner { get; set; }
+        public double ItemBaseBarcode { get; set; }
+        public string ItemBaseUnit { get; set; }
+        public double ItemBaseQuantity { get; set; }
+        public double ItemBaseNumberOfPcsInUnit { get; set; }
+        public string ItemBasePackageUnit { get; set; }
+        public string ItemBasePackageType { get; set; }
+        public double ItemBaseNetWeight { get; set; }
+        public double ItemBaseGrossWeight { get; set; }
+        public string ItemBaseWeightUnit { get; set; }
+        public double ItemBaseLength { get; set; }
+        public double ItemBaseWidth { get; set; }
+        public double ItemBaseHeight { get; set; }
+        public double ItemBaseVolume { get; set; }
+        public string ItemBaseVolumeUnit { get; set; }
+        public double ItemMiddle1Barcode { get; set; }
+        public double ItemMiddle1Quantity { get; set; }
+        public string ItemMiddle1PackageUnit { get; set; }
+        public double ItemMiddle1NetWeight { get; set; }
+        public double ItemMiddle1GrossWeight { get; set; }
+        public double ItemMiddle1Length { get; set; }
+        public double ItemMiddle1Width { get; set; }
+        public double ItemMiddle1Height { get; set; }
+        public double ItemMiddle1Volume { get; set; }
+        public string ItemMiddle1VolumeUnit { get; set; }
+        public double ItemMiddle2Barcode { get; set; }
+        public double ItemMiddle2Quantity { get; set; }
+        public string ItemMiddle2PackageUnit { get; set; }
+        public double ItemMiddle2NetWeight { get; set; }
+        public double ItemMiddle2GrossWeight { get; set; }
+        public double ItemMiddle2Length { get; set; }
+        public double ItemMiddle2Width { get; set; }
+        public double ItemMiddle2Height { get; set; }
+        public double ItemMiddle2Volume { get; set; }
+        public string ItemMiddle2VolumeUnit { get; set; }
+        public double ItemTopBarcode { get; set; }
+        public double ItemTopQuantity { get; set; }
+        public string ItemTopPackageUnit { get; set; }
+        public double ItemTopNetWeight { get; set; }
+        public double ItemTopGrossWeight { get; set; }
+        public string ItemTopWeightUnit { get; set; }
+        public double ItemTopLenght { get; set; }
+        public double ItemTopWidth { get; set; }
+        public double ItemTopHeight { get; set; }
+        public double ItemTopVolume { get; set; }
+        public string ItemTopVolumeUnit { get; set; }
+        public string ItemPackagingText { get; set; }
+        public double ItemBasePlasticMaterial { get; set; }
+        public double ItemBaseCartonMaterial { get; set; }
+        public double ItemBaseMetalMaterial { get; set; }
+        public double ItemBaseWoodenMaterial { get; set; }
+        public double ItemMiddle1PlasticMaterial { get; set; }
+        public double ItemMiddle1CartonMaterial { get; set; }
+        public double ItemMiddle1MetalMaterial { get; set; }
+        public double ItemMiddle1WoodenMaterial { get; set; }
+        public double ItemMiddle2PlasticMaterial { get; set; }
+        public double ItemMiddle2CartonMaterial { get; set; }
+        public double ItemMiddle2MetalMaterial { get; set; }
+        public double ItemMiddle2WoodenMaterial { get; set; }
+        public double ItemPlasticMaterialTop { get; set; }
+        public double ItemCartonMaterialTop { get; set; }
+        public double ItemMetalMaterialTop { get; set; }
+        public double ItemWoodenMaterialTop { get; set; }
+        public double ItemSupplierNumber { get; set; }
+        public double ItemProductionLeadTime { get; set; }
+        public double ItemPurchasingMinimumOrderQuantity { get; set; }
+        public double ItemProductPrice { get; set; }
+        public string ItemProductPriceCurrency { get; set; }
+        public double ItemPackagingPrice { get; set; }
+        public string ItemPackagingPurchasingCurrency { get; set; }
+        public double ItemCompleteProductPrice { get; set; }
+        public string ItemCompleteProductPurchasingCurrency { get; set; }
+        public double ItemMarginPRBNet { get; set; }
+        public double ItemPRBPriceUSD { get; set; }
+        public double ItemBCPriceUSD { get; set; }
+        public string ItemPriceList { get; set; }
+        public double ItemCompleteProductPriceUSD { get; set; }
+    }
+}
