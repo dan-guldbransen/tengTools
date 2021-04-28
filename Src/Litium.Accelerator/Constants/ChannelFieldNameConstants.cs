417af05c3fcea02140babe980e4e78e2486a7200
Andreas Crona <andreas.crona@consid.se>
2021-04-28 07:13:05 +0000

    Merged PR 18229: TENG-219
    SoME links

---
+++ Src/Litium.Accelerator/Constants/ChannelFieldNameConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Litium.Accelerator.Constants
+{
+    public class ChannelFieldNameConstants
+    {
+        public const string InstagramLink = "InstagramLink";
+        public const string Youtube = "Youtube";
+        public const string Twitter = "Twitter";
+        public const string Facebook = "Facebook";
+        public const string LinkedIn = "LinkedIn";
+    }
+}
---
+++ Src/Litium.Accelerator/Definitions/Channels/ChannelFieldDefinitionSetup.cs
@@ -0,0 +1,59 @@
+﻿using Litium.Accelerator.Constants;
+using Litium.FieldFramework;
+using Litium.FieldFramework.FieldTypes;
+using Litium.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Litium.Accelerator.Definitions.Channels
+{
+    public class ChannelFieldDefinitionSetup : FieldDefinitionSetup
+    {
+        public override IEnumerable<FieldDefinition> GetFieldDefinitions()
+        {
+            var fields = new[]
+            {
+                 new FieldDefinition<GlobalizationArea>(ChannelFieldNameConstants.InstagramLink, SystemFieldTypeConstants.Text)
+                {
+                    CanBeGridColumn = false,
+                    CanBeGridFilter = false,
+                    MultiCulture = false,
+                    Option = new TextOption()
+                },
+                  new FieldDefinition<GlobalizationArea>(ChannelFieldNameConstants.Youtube, SystemFieldTypeConstants.Text)
+                {
+                    CanBeGridColumn = false,
+                    CanBeGridFilter = false,
+                    MultiCulture = false,
+                    Option = new TextOption()
+                },
+                   new FieldDefinition<GlobalizationArea>(ChannelFieldNameConstants.Twitter, SystemFieldTypeConstants.Text)
+                {
+                    CanBeGridColumn = false,
+                    CanBeGridFilter = false,
+                    MultiCulture = false,
+                    Option = new TextOption()
+                },
+                    new FieldDefinition<GlobalizationArea>(ChannelFieldNameConstants.Facebook, SystemFieldTypeConstants.Text)
+                {
+                    CanBeGridColumn = false,
+                    CanBeGridFilter = false,
+                    MultiCulture = false,
+                    Option = new TextOption()
+                },
+                     new FieldDefinition<GlobalizationArea>(ChannelFieldNameConstants.LinkedIn, SystemFieldTypeConstants.Text)
+                {
+                    CanBeGridColumn = false,
+                    CanBeGridFilter = false,
+                    MultiCulture = false,
+                    Option = new TextOption()
+                },
+            };
+
+            return fields;
+        }
+    }
+}
