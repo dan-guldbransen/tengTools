2a20954fb61cf0c91e8aa7c8133ea09fc6133b97
Magnus Silfverala <magnus.silfverala@consid.se>
2021-05-17 09:23:43 +0200

    contactus VMBuilder updat

---
+++ Src/Litium.Accelerator/Builders/ContactUs/ContactUsViewModelBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Litium.Accelerator.Constants;
+using Litium.Accelerator.Routing;
+using Litium.Accelerator.ViewModels.ContactUs;
+using Litium.Runtime.AutoMapper;
+using Litium.Web.Models.Websites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Litium.Accelerator.Builders.ContactUs
+{
+    public class ContactUsViewModelBuilder : IViewModelBuilder<ContactUsViewModel>
+    {
+        private readonly RequestModelAccessor _requestModelAccessor;
+
+        public ContactUsViewModelBuilder(RequestModelAccessor requestModelAccessor)
+        {
+            _requestModelAccessor = requestModelAccessor;
+        }
+
+        public virtual ContactUsViewModel Build(PageModel pageModel)
+        {
+            var model = pageModel.MapTo<ContactUsViewModel>();
+            return model;
+        }
+    }
+
+}
---
+++ Src/Litium.Accelerator/ViewModels/ContactUs/ContactUsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using JetBrains.Annotations;
+using Litium.Accelerator.Builders;
+using Litium.Accelerator.Constants;
+using Litium.Accelerator.Extensions;
+using Litium.Accelerator.Routing;
+using Litium.Runtime.AutoMapper;
+using Litium.Web.Models.Blocks;
+using Litium.Web.Models.Websites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Litium.Accelerator.ViewModels.ContactUs
+{
+    public class ContactUsViewModel : IViewModel, IAutoMapperConfiguration
+    {
+        public string Introduction { get; set; }
+        public string Title { get; set; }
+
+        [Required]
+        public string ContacterName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string ContacterEmail { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string ContacterPhone { get; set; }
+
+        [Required]
+        public string ContacterMessage { get; set; }
+
+        public string ThankYouMessage { get; set; }
+
+        [UsedImplicitly]
+        public void Configure(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<PageModel, ContactUsViewModel>()
+                 .ForMember(x => x.Introduction, m => m.MapFromField(PageFieldNameConstants.Introduction))
+                 .ForMember(x => x.Title, m => m.MapFromField(PageFieldNameConstants.Title))
+                 .ForMember(x => x.ContacterEmail, m => m.MapFromField(ContactUsFieldNameConstants.ContactUsEmail))
+                 .ForMember(x => x.ContacterName, m => m.MapFromField(ContactUsFieldNameConstants.ContactUsName))
+                 .ForMember(x => x.ContacterPhone, m => m.MapFromField(ContactUsFieldNameConstants.ContactUsPhone))
+                 .ForMember(x => x.ContacterMessage, m => m.MapFromField(ContactUsFieldNameConstants.ContactUsMessage));
+        }
+    }
+}
+
+
